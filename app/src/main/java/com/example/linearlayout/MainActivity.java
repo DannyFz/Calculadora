b3c1c5a58689a2f45687629e149c9eca7c9b0043
DannyFz <71345818+DannyFz@users.noreply.github.com>
2020-10-27 18:51:35 -0600

    Correcciones

--- app/src/main/java/com/example/linearlayout/MainActivity.java
+++ app/src/main/java/com/example/linearlayout/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,39 +14,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-    public void DosPuntosV(){
-        View L=findViewById(R.id.Respuesta);
-        TextView Respuesta=(TextView)L;
-        Respuesta.setText("");
-    }
-
-    public void miclick(View view){
-     Button btn=(Button)view;
-     String digito=btn.getText().toString();
-     View L=findViewById(R.id.Respuesta);
-     TextView Respuesta=(TextView)L;
-     Respuesta.append(digito);
-    }
-
-
-    /*public void miclick(view view) {
-
-        switch(view.getid()){
-            case R.id.btn1:
-                toast.makeText(this,"Hola a android", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.btn2:
-              TextView t=(TextView)findViewById(R.id.lbl1);
-              t.setText("Cambio titulo");
-                break;
-
-            case R.id.rd1:
-                break;
-        }
-
-    }*/
-
 }
